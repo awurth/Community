e9cb19d64da5fbd3a8aae6913c6e3109185c0107
Alexis Wurth <alexis.wurth57@gmail.com>
2017-09-02 15:39:49 +0200

    Installed and configured NelmioApiDocBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -38,6 +38,7 @@ class AppKernel extends Kernel
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
                 $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
                 $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+                $bundles[] = new Nelmio\ApiDocBundle\NelmioApiDocBundle();
             }
         }
 
