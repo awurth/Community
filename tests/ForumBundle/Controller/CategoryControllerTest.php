6d3789a2f4844ff03f21ba06018ee2df49464f8f
Alexis Wurth <alexis.wurth57@gmail.com>
2017-09-04 19:26:34 +0200

    Added tests for ForumBundle CategoryController

---
+++ tests/ForumBundle/Controller/CategoryControllerTest.php
@@ -0,0 +1,222 @@
+<?php
+
+namespace Tests\ForumBundle\Controller;
+
+use ForumBundle\Entity\Category;
+use Tests\WebTestCase;
+
+class CategoryControllerTest extends WebTestCase
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->emptyTable('forum_category');
+    }
+
+    public function testGetCategories()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/categories');
+        $response = $client->getResponse();
+
+        $this->assertIsOk($response);
+        $this->assertJsonResponse($response);
+        $this->assertEqualsJson([], $response->getContent());
+    }
+
+    public function testGetCategoryNotFound()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/categories/1');
+        $response = $client->getResponse();
+
+        $this->assertIsNotFound($response);
+        $this->assertJsonResponse($response);
+    }
+
+    public function testGetCategory()
+    {
+        $category = $this->createCategory();
+
+        $client = static::createClient();
+
+        $client->request('GET', '/categories/' . $category->getId());
+        $response = $client->getResponse();
+
+        $this->assertIsOk($response);
+        $this->assertJsonResponse($response);
+        $this->assertEqualsJson($category, $response->getContent());
+    }
+
+    public function testPostCategory()
+    {
+        $content = '{
+            "forum_category": {
+                "title": "Category title",
+                "description": "This is a category"
+            }
+        }';
+
+        $client = static::createClient();
+
+        $response = $this->post($client, '/categories', $content);
+
+        // Test Response
+        $this->assertIsCreated($response);
+        $this->assertJsonResponse($response, false);
+
+        // Test created Category
+        $client->request('GET', $response->headers->get('Location'));
+
+        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertContains('"title":"Category title"', $client->getResponse()->getContent());
+    }
+
+    /**
+     * @dataProvider wrongCategoryProvider
+     */
+    public function testPostCategoryWithErrors($category)
+    {
+        $client = static::createClient();
+
+        $response = $this->post($client, '/categories', $category);
+
+        $this->assertIsBadRequest($response);
+        $this->assertJsonResponse($response);
+    }
+
+    public function testPutCategory()
+    {
+        $client = static::createClient();
+
+        $categoryToUpdate = $this->createCategory('New category');
+
+        $createdCategory = $this->em->getRepository('ForumBundle:Category')->find($categoryToUpdate->getId());
+        $this->assertSame('New category', $createdCategory->getTitle());
+        $this->assertNull($createdCategory->getUpdatedAt());
+
+        $updatedCategory = '{
+            "forum_category": {
+                "title": "Updated category",
+                "description": "This is an updated category"
+            }
+        }';
+
+        $response = $this->put($client, '/categories/' . $categoryToUpdate->getId(), $updatedCategory);
+
+        // Test Response
+        $this->assertIsNoContent($response);
+
+        // Test updated Category
+        $this->em->clear();
+        $updatedCategory = $this->em->getRepository('ForumBundle:Category')->find($categoryToUpdate->getId());
+
+        $this->assertSame('Updated category', $updatedCategory->getTitle());
+        $this->assertNotNull($updatedCategory->getUpdatedAt());
+    }
+
+    /**
+     * @dataProvider wrongCategoryProvider
+     */
+    public function testPutCategoryWithErrors($category)
+    {
+        $client = static::createClient();
+
+        $categoryToUpdate = $this->createCategory();
+
+        $response = $this->put($client, '/categories/' . $categoryToUpdate->getId(), $category);
+
+        $this->assertIsBadRequest($response);
+        $this->assertJsonResponse($response);
+    }
+
+    public function testPutCategoryNotFound()
+    {
+        $client = static::createClient();
+
+        $response = $this->put($client, '/categories/1', '');
+
+        $this->assertIsNotFound($response);
+        $this->assertJsonResponse($response);
+    }
+
+    public function testDeleteCategory()
+    {
+        $client = static::createClient();
+
+        $categoryToDelete = $this->createCategory('Category to delete');
+
+        $createdCategory = $this->em->getRepository('ForumBundle:Category')->find($categoryToDelete->getId());
+        $this->assertSame('Category to delete', $createdCategory->getTitle());
+        $this->assertNull($createdCategory->getUpdatedAt());
+
+        $client->request('DELETE', '/categories/' . $categoryToDelete->getId());
+        $response = $client->getResponse();
+
+        // Test Response
+        $this->assertIsNoContent($response);
+
+        $this->em->clear();
+        $deletedCategory = $this->em->getRepository('ForumBundle:Category')->find($categoryToDelete->getId());
+
+        $this->assertNull($deletedCategory);
+    }
+
+    public function testDeleteCategoryNotFound()
+    {
+        $client = static::createClient();
+
+        $client->request('DELETE', '/categories/1');
+        $response = $client->getResponse();
+
+        $this->assertIsNotFound($response);
+        $this->assertJsonResponse($response);
+    }
+
+    /**
+     * Creates a new Category.
+     *
+     * @param string $title
+     * @param string $description
+     *
+     * @return Category
+     */
+    public function createCategory($title = null, $description = null)
+    {
+        $category = new Category();
+        $category->setTitle($title ? $title : 'Category title');
+        $category->setDescription($description ? $description : 'This is a category');
+
+        $this->em->persist($category);
+        $this->em->flush();
+
+        return $category;
+    }
+
+    public function wrongCategoryProvider()
+    {
+        $category = '{
+            "forum_category": {
+                "title": "%s",
+                "description": "%s"
+            }
+        }';
+
+        $longString = str_repeat('a', 101);
+
+        return [
+            [sprintf($category, $longString, 'aaaa')],
+            [sprintf($category, $longString, '')],
+            [sprintf($category, 'aaaa', $longString)],
+            [sprintf($category, '', $longString)],
+            [sprintf($category, '', '')],
+            [sprintf($category, $longString, $longString)]
+        ];
+    }
+}
---
+++ tests/WebTestCase.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace Tests;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class WebTestCase extends BaseWebTestCase
+{
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setUp()
+    {
+        self::bootKernel();
+
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    /**
+     * Asserts that a PHP value corresponds to the given JSON.
+     *
+     * @param mixed  $expected
+     * @param string $actual
+     */
+    public function assertEqualsJson($expected, $actual)
+    {
+        $serializer = static::$kernel->getContainer()
+            ->get('jms_serializer');
+
+        $this->assertEquals($serializer->serialize($expected, 'json'), $actual);
+    }
+
+    /**
+     * Asserts that the response status code is 400 (Bad Request).
+     *
+     * @param Response $response
+     */
+    public function assertIsBadRequest(Response $response)
+    {
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $response->getStatusCode());
+    }
+
+    /**
+     * Asserts that the response status code is 201 (Created).
+     *
+     * @param Response $response
+     * @param bool     $checkLocationHeader
+     */
+    public function assertIsCreated(Response $response, $checkLocationHeader = true)
+    {
+        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
+        if ($checkLocationHeader) {
+            $this->assertNotNull($response->headers->get('Location'));
+        }
+    }
+
+    /**
+     * Asserts that the response status code is 204 (No Content).
+     *
+     * @param Response $response
+     * @param bool     $checkContent
+     */
+    public function assertIsNoContent(Response $response, $checkContent = true)
+    {
+        $this->assertEquals(Response::HTTP_NO_CONTENT, $response->getStatusCode());
+        if ($checkContent) {
+            $this->assertEmpty($response->getContent());
+        }
+    }
+
+    /**
+     * Asserts that the response status code is 404 (Not Found).
+     *
+     * @param Response $response
+     */
+    public function assertIsNotFound(Response $response)
+    {
+        $this->assertTrue($response->isNotFound());
+    }
+
+    /**
+     * Asserts that the response status code is 200.
+     *
+     * @param Response $response
+     */
+    public function assertIsOk(Response $response)
+    {
+        $this->assertTrue($response->isOk());
+    }
+
+    /**
+     * Asserts that the response's Content-Type is JSON.
+     *
+     * @param Response $response
+     * @param bool     $checkContent
+     */
+    public function assertJsonResponse(Response $response, $checkContent = true)
+    {
+        $this->assertTrue($response->headers->contains('Content-Type', 'application/json'));
+        if ($checkContent) {
+            $this->assertJson($response->getContent());
+        }
+    }
+
+    /**
+     * Deletes all rows of a table.
+     *
+     * @param string $name
+     */
+    public function emptyTable($name)
+    {
+        $this->em->getConnection()->executeUpdate('DELETE FROM ' . $name);
+    }
+
+    /**
+     * Sends a POST request.
+     *
+     * @param Client $client
+     * @param string $uri
+     * @param string $content
+     * @param array  $parameters
+     * @param array  $files
+     *
+     * @return Response|null
+     */
+    public function post(Client $client, $uri, $content, array $parameters = [], array $files = [])
+    {
+        $client->request('POST', $uri, $parameters, $files, [
+            'CONTENT_TYPE' => 'application/json'
+        ], $content);
+
+        return $client->getResponse();
+    }
+
+    /**
+     * Sends a PUT request.
+     *
+     * @param Client $client
+     * @param string $uri
+     * @param string $content
+     * @param array  $parameters
+     * @param array  $files
+     *
+     * @return Response|null
+     */
+    public function put(Client $client, $uri, $content, array $parameters = [], array $files = [])
+    {
+        $client->request('PUT', $uri, $parameters, $files, [
+            'CONTENT_TYPE' => 'application/json'
+        ], $content);
+
+        return $client->getResponse();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->em->close();
+        $this->em = null;
+    }
+}
