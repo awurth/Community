ec8ec00b4719d44e3525b656f7efa5cd57b431ea
Alexis Wurth <alexis.wurth57@gmail.com>
2017-09-05 14:15:49 +0200

    Added category to forum type

--- src/ForumBundle/Form/Type/ForumType.php
+++ src/ForumBundle/Form/Type/ForumType.php
@@ -2,6 +2,7 @@
 
 namespace ForumBundle\Form\Type;
 
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -17,7 +18,10 @@ class ForumType extends AbstractType
     {
         $builder
             ->add('title', TextType::class)
-            ->add('description', TextareaType::class);
+            ->add('description', TextareaType::class)
+            ->add('category', EntityType::class, [
+                'class' => 'ForumBundle:Category'
+            ]);
     }
     
     /**
