57ed26d3102db493a704146c3ecdb19039cb8ce0
Alexis Wurth <alexis.wurth57@gmail.com>
2017-09-06 14:33:37 +0200

    Fixed setter method name

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -81,7 +81,7 @@ class User extends BaseUser
      *
      * @return $this
      */
-    public function setUpdateAt(DateTime $updatedAt)
+    public function setUpdatedAt(DateTime $updatedAt)
     {
         $this->updatedAt = $updatedAt;
 
