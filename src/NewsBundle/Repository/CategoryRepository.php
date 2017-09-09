8a5bf7d0d79d05143adf4895452e773e845ee89d
Alexis Wurth <alexis.wurth57@gmail.com>
2017-09-09 20:18:59 +0200

    Added repository method for news categories

--- src/NewsBundle/Repository/CategoryRepository.php
+++ src/NewsBundle/Repository/CategoryRepository.php
@@ -6,4 +6,22 @@ use AppBundle\Repository\PaginationRepository;
 
 class CategoryRepository extends PaginationRepository
 {
+    /**
+     * Gets a paginated list of Categories.
+     *
+     * @param int    $perPage
+     * @param int    $page
+     * @param string $order
+     *
+     * @return array
+     */
+    public function getCollection($perPage = 15, $page = 1, $order = 'asc')
+    {
+        $qb = $this->createQueryBuilder('c')
+            ->orderBy('c.id', $order);
+
+        $qb = $this->paginate($qb, $perPage, $page);
+
+        return $qb->getQuery()->getResult();
+    }
 }
