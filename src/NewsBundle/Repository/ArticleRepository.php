be8950e103355208304000f58b43ddff1946c486
Alexis Wurth <alexis.wurth57@gmail.com>
2017-09-09 20:51:17 +0200

    Added repository method for articles pagination

--- src/NewsBundle/Repository/ArticleRepository.php
+++ src/NewsBundle/Repository/ArticleRepository.php
@@ -6,4 +6,22 @@ use AppBundle\Repository\PaginationRepository;
 
 class ArticleRepository extends PaginationRepository
 {
+    /**
+     * Gets a paginated list of Articles.
+     *
+     * @param int    $perPage
+     * @param int    $page
+     * @param string $order
+     *
+     * @return array
+     */
+    public function getCollection($perPage = 15, $page = 1, $order = 'asc')
+    {
+        $qb = $this->createQueryBuilder('a')
+            ->orderBy('a.id', $order);
+
+        $qb = $this->paginate($qb, $perPage, $page);
+
+        return $qb->getQuery()->getResult();
+    }
 }
