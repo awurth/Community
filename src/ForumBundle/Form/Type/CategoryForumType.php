5de229e6cc7b90068e7c5ed303b5e730e3769df8
Alexis Wurth <alexis.wurth57@gmail.com>
2017-09-09 15:42:08 +0200

    Removed unused 'use' statements

--- src/ForumBundle/Form/Type/CategoryForumType.php
+++ src/ForumBundle/Form/Type/CategoryForumType.php
@@ -2,12 +2,8 @@
 
 namespace ForumBundle\Form\Type;
 
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CategoryForumType extends AbstractType
 {
