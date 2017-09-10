2c1d06b42095404438294255d8e0374804c69279
Alexis Wurth <alexis.wurth57@gmail.com>
2017-09-10 18:53:12 +0200

    Added homepage

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations as Rest;
+
+class HomeController extends RestController
+{
+    /**
+     * @Rest\View
+     */
+    public function getAction()
+    {
+        return [
+            'forum' => [
+                'categories' => $this->generateUrl('get_forum_categories'),
+                'forums'     => $this->generateUrl('get_forum_forums'),
+                'topics'     => $this->generateUrl('get_forum_topics'),
+                'posts'      => $this->generateUrl('get_forum_posts')
+            ],
+            'news' => [
+                'categories' => $this->generateUrl('get_news_categories'),
+                'articles'   => $this->generateUrl('get_news_articles')
+            ],
+            'oauth' => $this->generateUrl('fos_oauth_server_token'),
+            'users' => $this->generateUrl('get_users')
+        ];
+    }
+}
