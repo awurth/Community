93975cdac8475182fba29f777caa6a74f01e8049
Alexis Wurth <alexis.wurth57@gmail.com>
2017-09-10 17:23:50 +0200

    Added NewsBundle data fixtures

---
+++ src/NewsBundle/DataFixtures/ORM/LoadArticleData.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace NewsBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use NewsBundle\Entity\Article;
+use NewsBundle\Entity\Category;
+use UserBundle\Entity\User;
+
+class LoadArticleData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        /** @var User $admin */
+        $admin = $this->getReference('admin');
+
+        for ($i = 1; $i <= 3; $i++) {
+            /** @var Category $category */
+            $category = $this->getReference("news-category-$i");
+
+            for ($j = 1; $j <= 3; $j++) {
+                $article = new Article();
+                $article->setTitle("Category $i - Article $j");
+                $article->setContent("This is the article $j of category $i");
+                $article->addCategory($category);
+                $article->setAuthor($admin);
+
+                $manager->persist($article);
+
+                $this->addReference("news-category-$i-article-$j", $article);
+            }
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 3;
+    }
+}
---
+++ src/NewsBundle/DataFixtures/ORM/LoadCategoryData.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace NewsBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use NewsBundle\Entity\Category;
+
+class LoadCategoryData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 1; $i <= 3; $i++) {
+            $category = new Category();
+            $category->setName("Category $i");
+            $category->setDescription("This is the category $i");
+
+            $manager->persist($category);
+
+            $this->addReference("news-category-$i", $category);
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 2;
+    }
+}
